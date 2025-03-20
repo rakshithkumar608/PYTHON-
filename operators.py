e9e7d3ae9ce8554ec64fdabfed2ce4460532dc7d
rakshithkumar608 <rakshithkumab01718@gmail.com>
2025-03-20 17:02:35 +0000

    first commit

---
+++ operators.py
@@ -0,0 +1,80 @@
+##operators
+
+##Assignment operator
+x = 10
+x += 100 # a = a + 100 short form
+
+y = 100
+y -= 10
+
+a = 30
+a *= 12
+
+print(x)
+print(y)
+print(a)
+
+## Comparision operators
+
+s = 10
+p = 30
+
+print(s == p)
+print(s > p)
+print(s < p)
+print(s != p)
+
+##Logical operators
+
+#1.And
+#2.or
+#3.Not
+
+p = 5
+q = 45
+r = 34
+
+print(p > 45 and q > 5)
+
+print(p > 20 or q > 45)
+
+print(not(p > 5))
+
+##Membership operator
+
+# in 
+# not in
+my_list = [1, 2, 3, 4, 5]
+my_string = "React"
+
+print(4 in my_list)
+print(("e" in my_string) and (2 in my_list))
+print((6 in my_list) and ("s" not in my_string))
+
+
+#Bitwise operator
+
+# (&)==AND
+# (|)==OR
+# (^)==XOR
+# (~)==NOT
+# (<<)==Left shift
+# (>>)==Right Shift
+
+e = 6
+f = 2
+
+print(e & f)
+print(e | f)
+print(f ^ e)
+print(f << e)
+
+##Arithematic operator
+
+# + (Addition)
+# - (Subtraction)
+# * (Multiplication)
+# / (Division)
+# // (Floor Division)
+# % (Modulus)
+# ** (Exponentiation)
